07ee49aedf0cc891c690d4d25d90c2533a5ac74b
karol <kstola2@uic.edu>
2018-12-06 14:07:21 -0600

    Added GUI implementation templates

---
+++ src/com/company/GUI_1.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class GUI_1 implements UserInterface {
+    @Override
+    public void display(String p) {
+
+    }
+
+    @Override
+    public String getLine() {
+        return null;
+    }
+}
--- src/com/company/GUI_apate.java
+++ src/com/company/GUI_apate.java
@@ -1,3 +1,7 @@
+package com.company;
+
+import com.company.Place;
+
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
